52037d08f9beb6d5d8f3000c0624e8501549d79f
Nikita <gnm11@yandex.ru>
2021-11-23 22:06:52 +0300

    Learn variables, part 2

---
+++ src/Primitives.java
@@ -0,0 +1,19 @@
+public class Primitives {
+    public static void main(String[] args) {
+
+        int first;
+        first=387;
+
+        int second=569;
+        int multiply=first*second;
+        System.out.println(multiply);
+
+        double total = 4563.32;
+        double count=4; //4.0
+        double price = total/count;
+
+        System.out.println("Price: "+ price); //Конкатенация строк - соединение двух строк (переменная преобразовалась в строку)
+
+
+    }
+}
